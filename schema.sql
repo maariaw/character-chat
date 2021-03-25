88f8b621a5b70baf5666fd15a6e5e6d879abe950
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2021-03-26 00:26:42 +0200

    Add SQL schema

---
+++ schema.sql
@@ -0,0 +1,45 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  password TEXT,
+  role INTEGER,
+  visible INTEGER
+);
+
+CREATE TABLE campaigns (
+  id SERIAL PRIMARY KEY,
+  title TEXT,
+  creator_id INTEGER REFERENCES users,
+  created_at TIMESTAMP,
+  password TEXT,
+  visible INTEGER
+);
+
+CREATE TABLE chats (
+  id SERIAL PRIMARY KEY,
+  title TEXT,
+  campaign_id INTEGER REFERENCES campaigns,
+  created_at TIMESTAMP,
+  visible INTEGER
+);
+
+CREATE TABLE messages (
+  id SERIAL PRIMARY KEY,
+  message TEXT,
+  user_id INTEGER REFERENCES users,
+  chat_id INTEGER REFERENCES chats,
+  sent_at TIMESTAMP,
+  visible INTEGER
+);
+
+CREATE TABLE campaign_users (
+  user_id INTEGER REFERENCES users,
+  campaign_id INTEGER REFERENCES campaigns,
+  visible INTEGER
+);
+
+CREATE TABLE "chat_users" (
+  user_id INTEGER REFERENCES users,
+  chat_id INTEGER REFERENCES chats,
+  visible INTEGER
+);
