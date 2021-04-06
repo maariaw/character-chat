4220c0c39075a4bd428c6150944457db8b83a6b4
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2021-04-06 15:33:46 +0300

    Make creating new campaigns possible for GMs

---
+++ campaigns.py
@@ -0,0 +1,6 @@
+from app import db
+
+def get_created_campaigns(user_id):
+    sql = """SELECT id, title, created_at FROM campaigns
+             WHERE creator_id=:user_id ORDER BY created_at"""
+    return db.session.execute(sql, {"user_id":user_id}).fetchall()
--- routes.py
+++ routes.py
@@ -2,16 +2,21 @@ from app import app
 from app import db
 from flask import redirect, render_template, request, session
 from werkzeug.security import check_password_hash, generate_password_hash
+import campaigns
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if session.get("role", 0) == 2:
+        campaign_list = campaigns.get_created_campaigns(session.get("user_id", 0))
+    else:
+        campaign_list = []
+    return render_template("index.html", campaigns=campaign_list)
 
 @app.route("/login", methods=["POST"])
 def login():
     username = request.form["username"]
     password = request.form["password"]
-    sql = "SELECT password, role FROM users WHERE name=:username"
+    sql = "SELECT password, id, role FROM users WHERE name=:username"
     result = db.session.execute(sql, {"username":username})
     user = result.fetchone()
     if user == None:
@@ -20,7 +25,8 @@ def login():
         hash_value = user[0]
         if check_password_hash(hash_value, password):
             session["username"] = username
-            role = user[1]
+            session["user_id"] = user[1]
+            role = user[2]
             session["role"] = role
             return redirect("/")
         else:
@@ -48,4 +54,26 @@ def register():
     db.session.commit()
     session["username"] = username
     session["role"] = account_type
+    return redirect("/")
+
+@app.route("/newcampaign")
+def new_campaign():
+    return render_template("newcampaign.html")
+
+@app.route("/createcampaign", methods=["POST"])
+def create_campaign():
+    title = request.form["title"]
+    password = request.form["password"]
+    if len(title) > 100:
+        return render_template("newcampaign.html", error="Title is too long")
+    if len(password) < 8:
+        return render_template("newcampaign.html", error="Password is too short")
+    if len(password) > 32:
+        return render_template("newcampaign.html", error="Password is too long")
+    hash_value = generate_password_hash(password)
+    creator_id = session.get("user_id", 0)
+    sql = """INSERT INTO campaigns (title, creator_id, created_at, password)
+             VALUES (:title, :creator_id, NOW(), :password)"""
+    db.session.execute(sql, {"title":title, "creator_id":creator_id, "password":hash_value})
+    db.session.commit()
     return redirect("/")
