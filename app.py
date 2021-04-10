64cb229e1cd19d53bc741587499e4153c509a49f
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2021-04-10 14:55:15 +0300

    Refactor to split app.py into app.py and db.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-db = SQLAlchemy(app)
 
 import routes
